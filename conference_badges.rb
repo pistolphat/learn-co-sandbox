ba334160a6245c13549b043ce7f38cc8a5abf8c5
Phat Doan <phatdoan@yahoo.com>
2018-09-30 19:51:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,6 @@ def batch_badge_creater(name)
   puts "name[0], name[1], name[2], name[3], name[4], name[5], name[6]"
 end
 
-
 def assign_rooms(room_number)
   room_number = ["1", "2", "3", "4", "5", "6", "7"]
     room_number.each do |room_number|
