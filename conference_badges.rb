4f827a25f2040de6021b94530323f1e418313a80
Phat Doan <phatdoan@yahoo.com>
2018-09-30 19:34:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   puts "Hello, my name is #{name)."
 end
 
-def batch_badge_creater
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  puts "#{names}"
+def batch_badge_creater(name)
+  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  puts "#{name}"
 end
 
 
@@ -12,7 +12,7 @@ def assign_rooms(room_number)
   room_number = ["1", "2", "3", "4", "5", "6", "7"]
     room_number.each do |room_number|
     end
-puts "Hello, #{names}! You'll be assigned to room #{room_number}"
+puts "Hello, #{name}! You'll be assigned to room #{room_number}"
 end
 
 def printer
