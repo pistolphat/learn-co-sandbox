9b1eeb60fc0ed14aea0a68240897a0b0c023236f
Phat Doan <phatdoan@yahoo.com>
2018-09-30 19:55:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def assign_rooms(room_number)
   room_number = ["1", "2", "3", "4", "5", "6", "7"]
     room_number.each do |room_number|
-    end
+  end
 puts "Hello, #{name}! You'll be assigned to room #{room_number}"
 end
 
