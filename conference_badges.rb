61d1a78cfdcc39a240ae4dada40bb4fb0f28b651
Phat Doan <phatdoan@yahoo.com>
2018-09-30 19:22:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,12 +2,18 @@ def badge_maker(name)
   puts "Hello, my name is #{name)."
 end
 
+
+
 def batch_badge_creater
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "#{names}"
 end
 
+
+
 def assign_rooms
+  
+
 
 puts "Hello, #{names}! You'll be assigned to room #{numbers}"
 end
